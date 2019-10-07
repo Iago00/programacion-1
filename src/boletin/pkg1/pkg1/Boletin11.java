e916fe87bb784059c1f8046b084d37b477226f26
Iago Pampillón Vieitez <ipampillonvieitez@nautilus27.ad.danielcastelao.org>
2019-10-07 09:12:21 +0200

    boletin1.1

---
+++ src/boletin/pkg1/pkg1/Boletin11.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg1.pkg1;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Boletin11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //area
+    float base = 4, altura = 3;
+    System.out.println("area = "+ (4*3/2));
+    
+       
+    }
+    
+}
